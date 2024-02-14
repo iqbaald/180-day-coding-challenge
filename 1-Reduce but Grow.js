1473426829421f194fce8493c4d2743928e818df
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2024-02-14 09:30:25 +0700

    update description 1/180

--- 1-Reduce but Grow.js
+++ 1-Reduce but Grow.js
@@ -1,4 +1,6 @@
-/* 
+/*
+180 day Coding Challenge (1/180)
+Link : https://www.codewars.com/kata/57f780909f7e8e3183000078/solutions/javascript
 Question : Given a non-empty array of integers, return the result of multiplying the values together in order. 
 Input : [1, 2, 3, 4]
 Result : 1 * 2 * 3 * 4 = 24
