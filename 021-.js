6cdb9c128084b7c5488abf4787547c4d80c77c62
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2024-03-09 17:44:11 +0700

    day 20/180

---
+++ 021-.js
@@ -0,0 +1,9 @@
+/*
+180 day Coding Challenge (19/180)
+Link : https://www.codewars.com/kata/57eaeb9578748ff92a000009
+Question : Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+Input : ('Daniel', 'Daniel') || ('Greg', 'Daniel')
+Output : 'Hello boss' || 'Hello guest'
+*/
+
+// BARBAR ANSWER
