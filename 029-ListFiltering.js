4596dd742f8dc1a8ae236e136435b79430ac6e41
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2024-03-17 08:45:41 +0700

    day 29/180

---
+++ 029-ListFiltering.js
@@ -0,0 +1,36 @@
+/*
+180 day Coding Challenge (19/180)
+Link : https://www.codewars.com/kata/53dbd5315a3c69eed20002dd
+*/
+
+// BARBAR ANSWER
+
+function filter_list2(list) {
+  let result = [];
+  for (let i = 0; i < list.length; i++) {
+    if (typeof list[i] == "number") {
+      result.push(list[i]);
+    }
+  }
+  return result;
+}
+
+console.log(filter_list2([1, 2, "a", "b"]));
+console.log(filter_list2([1, "a", "b", 0, 15]));
+console.log(filter_list2([1, 2, "aasf", "1", "123", 123]));
+
+// ANOTHER ANSWER
+
+const filter_list3 = (list) => list.filter((arr) => typeof arr === "number");
+
+console.log(filter_list3([1, 2, "a", "b"]));
+console.log(filter_list3([1, "a", "b", 0, 15]));
+console.log(filter_list3([1, 2, "aasf", "1", "123", 123]));
+
+// CRAZY ANSWER
+// Refference : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
+const filter_list = (list) => list.filter(Number.isInteger);
+
+console.log(filter_list([1, 2, "a", "b"]));
+console.log(filter_list([1, "a", "b", 0, 15]));
+console.log(filter_list([1, 2, "aasf", "1", "123", 123]));
