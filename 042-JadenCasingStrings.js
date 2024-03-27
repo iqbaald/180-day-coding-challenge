bb265afcb076815d3d78730ceab7e0be8aad587d
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2024-03-27 19:47:29 +0700

    day 43/180

--- 042-JadenCasingStrings.js
+++ 042-JadenCasingStrings.js
@@ -15,5 +15,16 @@ String.prototype.toJadenCase = function () {
   return word.join(" ");
 };
 
-let str = "How Can Mirrors Be Real If Our Eyes Aren't Real";
+let str = "How can mirrors be real if our eyes aren't real";
 console.log(str.toJadenCase());
+
+// ONLINE ANSWER
+
+String.prototype.toJadenCase2 = function () {
+  return this.split(" ")
+    .map((item) => item[0].toUpperCase() + item.slice(1))
+    .join(" ");
+};
+
+let str2 = "How can mirrors be real if our eyes aren't real";
+console.log(str2.toJadenCase2());
