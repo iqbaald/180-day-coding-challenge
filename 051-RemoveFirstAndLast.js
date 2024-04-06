6b65512cd534916bbbcd093455f73ad0e554d7bf
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2024-04-06 13:23:20 +0700

    day 51/180

---
+++ 051-RemoveFirstAndLast.js
@@ -0,0 +1,14 @@
+/*
+180 day Coding Challenge (51/180)
+Link : https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
+
+*/
+
+// SLICE ANSWER
+
+function removeChar(str) {
+  return str.slice(1, -1);
+}
+
+console.log(removeChar("eloquent"));
+console.log(removeChar("country"));
