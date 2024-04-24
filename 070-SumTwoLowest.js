d57ac80dc7d37397a440ace9fbe88516a6cb48dd
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2024-04-25 04:54:09 +0700

    day 70/180

---
+++ 070-SumTwoLowest.js
@@ -0,0 +1,24 @@
+/*
+180 day Coding Challenge (70/180)
+Link : https://www.codewars.com/kata/558fc85d8fd1938afb000014
+
+  Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+*/
+
+// BARBAR ANSWER
+
+function sumTwoSmallestNumbers(numbers) {
+  let sortNumber = numbers.sort((a, b) => a - b);
+  return sortNumber[0] + sortNumber[1];
+}
+
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]));
+console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]));
+console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]));
+console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]));
+console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4]));
