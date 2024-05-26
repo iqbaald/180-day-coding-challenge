53df8a46244725a9b4392de2340b9ee06aaf5259
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2024-05-26 17:22:03 +0700

    day 76/180

---
+++ 076-ConvertStrToNmbr.js
@@ -0,0 +1,19 @@
+/*
+180 day Coding Challenge (76/180)
+Link : https://www.codewars.com/kata/544675c6f971f7399a000e79
+
+"1234" --> 1234
+"605"  --> 605
+"1405" --> 1405
+"-7" --> -7
+
+*/
+
+// BARBAR ANSWER
+const stringToNumber = function (str) {
+  return parseInt(str);
+};
+console.log(stringToNumber("1234"));
+console.log(stringToNumber("605"));
+console.log(stringToNumber("1405"));
+console.log(stringToNumber("-7"));
