dbaf72f559bf36a506f495e2d997fdf167338b27
Muhamad Iqbaal Dhoifulloh <iqbaaldhoifulloh@gmail.com>
2024-06-27 19:22:24 +0700

    day 86/180

---
+++ 086-AbbreviateTwoWord.js
@@ -0,0 +1,25 @@
+/*
+180 day Coding Challenge (86/180)
+Link : https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3
+
+Input -> Output
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+
+*/
+
+// BARBAR ANSWER
+function abbrevName(name) {
+  let singleName = name.split(" ");
+  return (
+    singleName[0].charAt(0).toUpperCase() +
+    "." +
+    singleName[1].charAt(0).toUpperCase()
+  );
+}
+
+console.log(abbrevName("sam harris"));
+console.log(abbrevName("atrick Feenan"));
+console.log(abbrevName("Evan Cole"));
